d5f053cbaf0a9fefeed63ea645d9992f873ecd02
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-22 21:26:54 -0400

    Adicionado Partial

--- TDSTecnologia.Site.Web/Controllers/HomeController.cs
+++ TDSTecnologia.Site.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TDSTecnologia.Site.Infrastructure.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,16 @@ namespace TDSTecnologia.Site.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.CursoDao.ToListAsync());
+        }
+
+        private readonly AppContexto _context;
+
+        public HomeController(AppContexto context)
+        {
+            _context = context;
         }
     }
 }
